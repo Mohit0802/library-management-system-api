ae665b56b6492cde7a1cb5a93b6e308daae7eadc
Mohit <mohittahiliani2019@gmail.com>
2026-02-09 23:10:52 +0530

    Added IsOwnerOrReadOnly custom permission for BookReview ownership control

--- library/permissions.py
+++ library/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsLibrarian(BasePermission):
     def has_permission(self, request, view):
@@ -8,3 +8,9 @@ class IsLibrarian(BasePermission):
 class IsStudent(BasePermission):
     def has_permission(self, request, view):
         return request.user.role == "STUDENT"
+
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.user == request.user
