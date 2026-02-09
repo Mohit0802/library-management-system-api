17c2c10524d21bf6ec8bda3f86620d94b0a95702
Mohit <mohittahiliani2019@gmail.com>
2026-02-09 19:26:46 +0530

    Implemented Author, Genre, Book, BorrowRequest, and BookReview models

--- library/models.py
+++ library/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class User(AbstractUser):
@@ -7,3 +8,64 @@ class User(AbstractUser):
         STUDENT = "STUDENT"
         LIBRARIAN = "LIBRARIAN"
     role = models.CharField(max_length=20, choices=Roles.choices, default=Roles.STUDENT)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    bio = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    genres = models.ManyToManyField(Genre)
+    ISBN = models.CharField(max_length=20)
+    available_copies = models.PositiveIntegerField(default=1)
+    total_copies = models.PositiveIntegerField(default=1)
+    
+    def __str__(self):
+        return self.title
+
+
+class BorrowRequest(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "PENDING"
+        APPROVED = "APPROVED"
+        REJECTED = "REJECTED"
+        RETURNED = "RETURNED"
+
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
+    requested_at = models.DateTimeField(auto_now_add=True)
+    approved_at = models.DateTimeField(null=True, blank=True)
+    returned_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title} ({self.status})"
+
+
+class BookReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField(
+        validators = [
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title} ({self.rating})"
