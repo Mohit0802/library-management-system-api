f04d273eb35840f99fcc09d2550c753b59413a82
Mohit <mohittahiliani2019@gmail.com>
2026-02-09 19:45:47 +0530

    Added custom permissions for librarian and student roles

---
+++ library/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission
+
+class IsLibrarian(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == "LIBRARIAN"
+
+
+class IsStudent(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == "STUDENT"
